3d3162510877f8345727310c5b58595cf612cc05
web_samurai <79477730+d0tsec@users.noreply.github.com>
2021-03-31 10:40:17 +0300

    Update ECP.js

--- ECP.js
+++ ECP.js
@@ -1,4 +1,4 @@
-window.CryptoHelper = function() {
+window.ECP = function() {
 	const self = this;
 	const crypto = cadesplugin;
 
@@ -7,10 +7,7 @@ window.CryptoHelper = function() {
 
 	self.isReady = false;
 
-	/**
-	 * Handle plugin init event
-	 * @returns {Promise}
-	 */
+	
 	this.init = () => {
 		return new Promise((resolve, reject) => {
 			crypto.then(async () => {
@@ -25,13 +22,7 @@ window.CryptoHelper = function() {
 		});
 	};
 
-	/**
-	 * Get valid certificates from store
-	 * @param {Number} location location of the store to be opened
-	 * @param {String} storeName a string that contains the name of the system certificate store to be opened
-	 * @param {Number} mode open mode of the store
-	 * @returns {Array<Certificate>}
-	 */
+	
 	this.getCertificates = (location, storeName, mode) => {
 		const _location = location || crypto.CAPICOM_CURRENT_USER_STORE;
 		const _storeName = storeName || crypto.CAPICOM_MY_STORE;
@@ -56,12 +47,7 @@ window.CryptoHelper = function() {
 		});
 	};
 
-	/**
-	 * Sign various data type by chosen certificate
-	 * @param {Certificate} certificate
-	 * @param {String|File|DomElement} data
-	 * @returns {String}
-	 */
+	
 	this.sign = (certificate, data) => {
 		if (data instanceof File) {
 			log('[Crypto] Signing File');
@@ -78,13 +64,7 @@ window.CryptoHelper = function() {
 		}
 	};
 
-	/**
-	 * Sign string data by chosen certificate
-	 * @param {Certificate} certificate
-	 * @param {String} data
-	 * @param {Boolean} toBase64
-	 * @returns {String}
-	 */
+	
 	this.signString = (certificate, data, toBase64 = true) => {
 		return new Promise(async (resolve, reject) => {
 			const signer = await createSigner();
@@ -106,12 +86,7 @@ window.CryptoHelper = function() {
 		});
 	};
 
-	/**
-	 * Sign file by chosen certificate
-	 * @param {Certificate} certificate
-	 * @param {File} file
-	 * @returns {String}
-	 */
+	
 	this.signFile = (certificate, file) => {
 		return new Promise((resolve) => {
 			const reader = new FileReader();
@@ -125,12 +100,7 @@ window.CryptoHelper = function() {
 		});
 	};
 
-	/**
-	 * Sign file list by chosen certificate
-	 * @param {Certificate} certificate
-	 * @param {FileList} fileList
-	 * @returns {Array<String>}
-	 */
+	
 	this.signFileList = (certificate, fileList) => {
 		const promises = Array.from(fileList).map((file) => {
 			return this.signFile(certificate, file);
@@ -139,13 +109,7 @@ window.CryptoHelper = function() {
 		return Promise.all(promises);
 	};
 
-	/**
-	 * Verify data sign
-	 * @param {String} data
-	 * @param {String} sign
-	 * @param {Boolean} toBase64
-	 * @returns {Array<Object>}
-	 */
+	
 	this.verify = (data, sign, toBase64 = false) => {
 		return new Promise(async (resolve) => {
 			const signedData = await createSignedData();
@@ -185,10 +149,7 @@ window.CryptoHelper = function() {
 		});
 	};
 
-	/**
-	 * Check cadesplugin is ready
-	 * (try create Store object)
-	 */
+	
 	async function check() {
 		try {
 			await createStore();
@@ -198,58 +159,34 @@ window.CryptoHelper = function() {
 		}
 	}
 
-	/**
-	 * Create new CAdESCOM.Store instance
-	 * View more at https://cpdn.cryptopro.ru/content/cades/class_store.html
-	 * @returns {CAdESCOM.Store}
-	 */
+	
 	async function createStore() {
 		return await crypto.CreateObjectAsync('CAdESCOM.Store');
 	}
 
-	/**
-	 * Create new CAdESCOM.CPSigner instance
-	 * View more at https://cpdn.cryptopro.ru/content/cades/class_c_ad_e_s_c_o_m_1_1_c_p_signer.html
-	 * @returns {CAdESCOM.CPSigner}
-	 */
+	
 	async function createSigner() {
 		return await crypto.CreateObjectAsync('CAdESCOM.CPSigner');
 	}
 
-	/**
-	 * Create new CAdESCOM.CadesSignedData instance
-	 * View more at https://cpdn.cryptopro.ru/content/cades/class_c_ad_e_s_c_o_m_1_1_cades_signed_data.html
-	 * @returns {CAdESCOM.CadesSignedData}
-	 */
+	
 	async function createSignedData() {
 		return await crypto.CreateObjectAsync('CAdESCOM.CadesSignedData');
 	}
 
-	/**
-	 * Extract certificate subject info
-	 * @param {Certificate} certificate
-	 * @returns {Object}
-	 */
+	
 	async function extractSubjectName(certificate) {
 		var subject = await certificate.SubjectName;
 		return parseDN(subject);
 	}
 
-	/**
-	 * Extract certificate issuer info
-	 * @param {Certificate} certificate
-	 * @return {Object}
-	 */
+	
 	async function extractIssuerName(certificate) {
 		var issuer = await certificate.IssuerName;
 		return parseDN(issuer);
 	}
 
-	/**
-	 * Extract certificate info
-	 * @param {Certificate} certificate
-	 * @returns {Object}
-	 */
+	
 	async function parseCertificate(certificate) {
 		const isValid = await certificate.IsValid();
 
@@ -267,11 +204,7 @@ window.CryptoHelper = function() {
 		}
 	}
 
-	/**
-	 * Extract signers from SignedData object
-	 * @param {CAdESCOM.CadesSignedData} signedData
-	 * @returns {Array<Object>}
-	 */
+	
 	async function signInfo(signedData) {
 		const signers = await signedData.Signers;
 		const count = await signers.Count;
@@ -293,11 +226,7 @@ window.CryptoHelper = function() {
 		return signs;
 	}
 
-	/**
-	 * Parse DN string to object
-	 * @param {String} dn
-	 * @returns {Object}
-	 */
+	
 	function parseDN(dn) {
 		const tags = {
 			'CN': 'name',
@@ -334,10 +263,7 @@ window.CryptoHelper = function() {
 		return fields;
 	}
 
-	/**
-	 * Write verbose message to console
-	 * @param  {...any} args
-	 */
+	
 	function log(...args) {
 		if (VERBOSE) {
 			console.log(...args);
